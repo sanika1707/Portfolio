98c5680b52462b284f4967fd3fd9949fd759f5d4
sanika1707 <sanikakhirit43@gmail.com>
2022-08-14 11:25:47 -0700

    Initial commit

---
+++ components/constants.js
@@ -0,0 +1,84 @@
+import LOGO from "../assets/images/Sanika_Khirit.png";
+import HTML from "../assets/images/Skills/Html.png";
+import CSS from "../assets/images/Skills/CSS.png";
+import JAVASCRIPT from "../assets/images/Skills/Javascript.png";
+import PYTHON from "../assets/images/Skills/Python.png";
+import NODE_JS from "../assets/images/Skills/Node_JS.png";
+import React_Logo from '../assets/images/Skills/React_Logo.png';
+
+const STUDENT_NAME = "Sanika Khirit";
+const FIRST_NAME = "Sanika";
+const MAIN_ROLE = "UI/UX Designer";
+const PHONE_NUMBER = "9172129958";
+const EMAIL = "sanikakhirit43@gmail.com";
+const LOCATION = "Akurdi, Pune.";
+const BLOB_IMG = "./sanika.jpg";
+const LINKEDIN_LINK = "https://www.linkedin.com/in/sanika-khirit/";
+const YOUTUBE_LINK = "";
+const GITHUB_LINK = "https://github.com/sanika1707";
+const INSTAGRAM_LINK = "https://www.instagram.com/sanika_khirit/";
+const ABOUT_DESCRIPTION = "I am a computer engineering student having interest in Full Stack Web Development";
+const INFO_DESCRIPTION = "Innovative and self-motivated computer engineering student with keen interest in programming and developing technical solutions for betterment of Society/Organization";
+const EMAIL_JS_SERVICE_ID = "service_hy098m9";
+const EMAIL_JS_TEMPLATE_ID = "template_ax4pffd";
+const EMAIL_JS_PUBLIC_KEY = "UmW-ItS757_9uPedl";
+
+const PROJECTS = [
+    {
+        name: "Result Generation and Analysis System",
+        github: "https://github.com/Dnyanesh-Bachhav/Result-Generation-and-Analysis-System",
+    },
+    {
+        name: "CoinStack",
+        github: "https://github.com/Dnyanesh-Bachhav/Crypto-Market",
+    },
+    {
+        name: "IPL App",
+        github: "https://github.com/Dnyanesh-Bachhav/IPL-app",
+    },
+    {
+        name: "Dynamic Shop Application",
+        github: "https://github.com/Dnyanesh-Bachhav/Dynamic-Shop-Application",
+    },
+    {
+        name: "Marathi Status App",
+        github: "https://github.com/Dnyanesh-Bachhav/Jarvis-in-Javascript",
+    }
+];
+const SKILLS = [
+    {
+        id: 1,
+        name: "HTML",
+        img_src: HTML,
+    },
+    
+    {
+        id: 2,
+        name: "CSS",
+        img_src: CSS,
+    },
+    {
+        id: 3,
+        name: "Javascript",
+        img_src: JAVASCRIPT,
+    },
+    {
+        id: 4,
+        name: "React_JS",
+        img_src: React_Logo,
+    },
+    {
+        id: 5,
+        name: "Node_JS",
+        img_src: NODE_JS,
+    },
+    {
+        id: 6,
+        name: "Python",
+        img_src: PYTHON,
+    }
+    
+    
+];
+
+export {STUDENT_NAME,ABOUT_DESCRIPTION,FIRST_NAME,MAIN_ROLE,LINKEDIN_LINK,YOUTUBE_LINK,GITHUB_LINK,INSTAGRAM_LINK,PHONE_NUMBER,EMAIL,LOCATION,LOGO,BLOB_IMG,PROJECTS,SKILLS,EMAIL_JS_SERVICE_ID,EMAIL_JS_TEMPLATE_ID,EMAIL_JS_PUBLIC_KEY,INFO_DESCRIPTION};
